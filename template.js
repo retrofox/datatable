98b4bf982fb00f3b16f356a18a2e891e481fb3a0
Damian Suarez <rdsuarez@gmail.com>
2012-10-12 09:01:57 -0300

    template: first compiled template

--- template.js
+++ template.js
@@ -1,1 +1,1 @@
-module.exports = '';
+module.exports = '<div class="data-table-component">\n  <table>\n    <thead>\n      <tr>\n        <th></th>\n      </tr>\n    </thead>\n\n    <tfoot>\n      <tr>\n        <td></td>\n      </tr>\n    </tfoot>\n\n    <tbody>\n      <tr>\n        <td></td>\n      </tr>\n    </tbody>\n  </table>\n</div>\n';
