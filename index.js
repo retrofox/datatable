7d28354a54d9f6543b6d9f5b0b07fc51de78d33e
Damian Suarez <rdsuarez@gmail.com>
2012-10-12 08:09:43 -0300

    core: add component dependencies

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Component dependencies
+ */
+
+var inherit = require('inherit')
+  , o = require('jquery')
+  , emitter = require('emitter')
+  , request = require('superagent')
+  , pager = require('pager');
