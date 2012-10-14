386363014fc12cd9e0a3bd092551b7d9aea4739b
Damian Suarez <rdsuarez@gmail.com>
2012-10-14 11:05:05 -0300

    ignore: remove template.hs from git cache

--- template.js
+++
@@ -1,1 +0,0 @@
-module.exports = '<div class="data-table-component">\n  <table>\n    <thead>\n      <tr>\n        <th></th>\n      </tr>\n    </thead>\n\n    <tfoot>\n      <tr>\n        <td></td>\n      </tr>\n    </tfoot>\n\n    <tbody>\n      <tr>\n        <td></td>\n      </tr>\n    </tbody>\n  </table>\n</div>\n';
