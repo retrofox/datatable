bfc748e6994045b52a1c608b1bf1d7508f87f149
Damian Suarez <rdsuarez@gmail.com>
2012-10-14 12:39:55 -0300

    core: add .add() and .addData() methods

--- index.js
+++ index.js
@@ -34,12 +34,35 @@ DataTable.prototype.__proto__ = Emitter.prototype;
 /**
  * Add new row to DataTable
  *
+ * @param {Array} row
  * @api public
  */
 
 DataTable.prototype.add = function(row){
-  
+  if (!row.length) return this;
+
+  for (var i = 0, tr = o('<tr>'); i < row.length; i++) {
+    tr.append(o('<td>', { text: row[i] }));
+  }
+  this.el.find('tbody').append(tr);
+  return this;
 };
+
+/**
+ * Add a lot of rows
+ *
+ *@param {Array} data
+ * @api public
+ */
+
+DataTable.prototype.addData = function(data){
+  if (!data.length) return this;
+  for (var i = 0; i < data.length; i++) {
+    this.add(data[i]);
+  }
+  return this;
+};
+
 
 /**
  * Set table header
@@ -49,6 +72,8 @@ DataTable.prototype.add = function(row){
  */
 
 DataTable.prototype.header = function(cols){
+  if (!cols.length) return this;
+
   for (var i = 0, c = cols[0]; i < cols.length; i++, c = cols[i]) {
     var isstr = 'string' == type(c);
     var text = isstr ? c : c[0];
